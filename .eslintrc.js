e1c224d10dc6880bfbbd6d0c47256242f92213aa
Melliim <ibrahim_meleha@hotmail.com>
2023-06-15 18:42:28 +0200

    1st dependencies update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    jest: true
   },
   extends: [
     'plugin:vue/vue3-essential',
@@ -19,10 +20,7 @@ module.exports = {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
         '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
+      ]
     }
   ]
 }
