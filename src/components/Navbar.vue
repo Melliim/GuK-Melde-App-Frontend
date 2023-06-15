6512b7e2111ecda1520db74486b1c80ac87bec4d
Melliim <ibrahim_meleha@hotmail.com>
2023-06-15 20:37:17 +0200

    add a simple navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<script>
+
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Navbar'
+}
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">GuK</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <router-link class="nav-link" to = "/">Home</router-link>
+                    <router-link class="nav-link" to = "/about">About</router-link>
+                    <router-link class="nav-link" to = "/extra">Extra</router-link>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Extra.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>This is Extra</h1>
+</template>
+
+<script >
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name : 'Extra'
+}
+</script>
